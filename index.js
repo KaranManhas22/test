fb9e79fe8489d9bf5e88d21e832d8f9cdb34626f
Karan Singh Manhas <karansingh33477@gmail.com>
2025-07-29 15:35:19 +0530

    commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const res = require('express/lib/response')
-const db = require('./server/config/db')
+// const db = require('./server/config/db')
 const cors = require('cors')
 const app = express()
 const PORT = 3004
